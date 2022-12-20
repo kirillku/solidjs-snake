b93d06e9978dde94c347f4223a46acaeb4fb8c3d
Kirill Kubryakov <kubryakov@gmail.com>
2022-12-20 18:21:34 +0200

    add size and speed to url search params (temp solution)

--- src/engine/settings.ts
+++ src/engine/settings.ts
@@ -1,13 +1,20 @@
 import { createSignal } from "solid-js";
 
+// TODO: move url management to app
+const searchParams = new URLSearchParams(window.location.search);
+const urlSize = searchParams.get("size");
+const urlSpeed = searchParams.get("speed");
+
 const DEFAULT_GAME_FIELD_SIZE = 10;
 
 const [gameFieldSize, setGameFieldSize] = createSignal<number>(
-  DEFAULT_GAME_FIELD_SIZE
+  urlSize ? parseInt(urlSize) : DEFAULT_GAME_FIELD_SIZE
 );
 
 const DEFAULT_GAME_SPEED = 120;
 
-const [gameSpeed, setGameSpeed] = createSignal<number>(DEFAULT_GAME_SPEED);
+const [gameSpeed, setGameSpeed] = createSignal<number>(
+  urlSpeed ? parseInt(urlSpeed) : DEFAULT_GAME_SPEED
+);
 
 export { gameFieldSize, setGameFieldSize, gameSpeed, setGameSpeed };
